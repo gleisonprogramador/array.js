a066711cd73de78437e89431b999139c745e2ccb
gleisonprogramador <137187327+gleisonprogramador@users.noreply.github.com>
2023-06-21 16:27:54 -0500

    yghuiughujfeugihdgfdfuhdfuhfudfhfud

---
+++ variavel24.js
@@ -0,0 +1,11 @@
+unction vogal(){
+    var letra = prompt("digite uma letra")
+
+if(letra == "a"|| letra == "e" || letra == "i" || letra == "o " || letra == "u"){
+      document.write("essa letra e uma vogal")
+}
+else{
+    document.write("essa letra não e uma vogal")
+}
+}
+vogal()e
